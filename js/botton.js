1811fb9e855d2a66aca5a14a15e85504dafe06bc
Satoshi628 <180442042@ccmailg.meijo-u.ac.jp>
2021-11-15 01:38:57 +0900

    introduction page

--- js/botton.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('img_open_botton').addEventListener("click",function(){
-  var image = document.getElementById('image');
-  if (image.style.display == 'none') {
-    image.style.display = 'block';
-  }else {
-    image.style.display = 'none';
-  }
-});
